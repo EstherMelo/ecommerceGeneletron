69dcdb00ac9a83802770707daeea8515157525f5
EstherMelo <emelo3172@gmail.com>
2021-09-04 20:27:27 -0300

    java

--- ecommerceGeneletronJava/src/ecommerGeneletronJava/principal.java
+++ ecommerceGeneletronJava/src/ecommerGeneletronJava/principal.java
@@ -299,20 +299,26 @@ public class principal {
 
                                 do {
                                     //limpa();
-                                    System.out.print("GENELETRON\n");
+                                    System.out.print("               GENELETRON\n");
                                     System.out.print("-----------------------------------------------\n");
                                     System.out.print("Gerando economia e qualidade pro seu dia a dia!\n\n");
 
                                     // 10.c) mostrar os dois
                                     System.out.print("Total da compra: R$ " + totalDaCompra +"\n");
                                     System.out.printf("Valor do Imposto sobre a Compra (9): R$ %.2f \n\n", impostoDaCompra);
-
+                                    System.out.print("-----------------------------------------------\n");
                                     // 10.d) exibir as formas e mostrar forma de pagamento
-                                    System.out.print("Escolha a forma de pagamento: \n");
-                                    System.out.print("OPÇÕES DE PAGAMENTO:\n");
-                                    System.out.print("1) Á Vista Com 10% Desconto.\n");
-                                    System.out.print("2) No cartão com 10% de acrescimo.\n");
-                                    System.out.print("3) Em duas vezes com 15% de taxa.\n");
+                                    
+                                    System.out.print("   Escolha a forma de pagamento: \n");
+                                    System.out.print("-----------------------------------------------\n");
+                                    System.out.print("      OPÇÕES DE PAGAMENTO:\n");
+                                    System.out.print(" __________________________________\n");
+                                    System.out.print("|1) Á Vista Com 10% Desconto       |\n");
+                                    System.out.print("|__________________________________|\n");
+                                    System.out.print("|2) No cartão com 10% de acrescimo |\n");
+                                    System.out.print("|__________________________________|\n");
+                                    System.out.print("|3) Em duas vezes com 15% de taxa  |\n");
+                                    System.out.print("|__________________________________|\n");
                                     formaDePagamento = leia.nextInt();
 
                                     // 10.e) de acordo com a forma de pagamento, calcular o totalAPagar
