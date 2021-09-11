bff034c962f4fefb02353859de45e265b2eaa84c
LucasPF10 <90116434+LucasPF10@users.noreply.github.com>
2021-09-10 22:31:46 -0300

    lucas

--- ecommerceGeneletronJavaPOO/src/app/TelaPrincipal.java
+++ ecommerceGeneletronJavaPOO/src/app/TelaPrincipal.java
@@ -1,10 +1,29 @@
 package app;
 
+import java.util.ArrayList;
+
+import entities.Produto;
+
 public class TelaPrincipal {
 
 	public static void main(String[] args) {
-		
+		ArrayList<Produto> product = new ArrayList<Produto>();
+		product.add(new Produto("G1-1", "Notebook Acer Gamer ", 4.700, 10));
+		product.add(new Produto("G1-2", "Smart TV LG 55´", 2.799, 10));
+		product.add(new Produto("G1-3", "Monitor 4k 32'", 2.500, 10));
+		product.add(new Produto("G1-4", "Smartphone Motorola", 1.799, 10));
+		product.add(new Produto("G1-5", "Cadeira gamer Thunderx3", 1.500, 10));
+		product.add(new Produto("G1-6", "Drone DGI", 5.300, 10));
+		product.add(new Produto("G1-7", "Xbox Séries S", 2.518, 10));
+		product.add(new Produto("G1-8", "Apple watch series 3", 1.500, 10));
+		product.add(new Produto("G1-9", "Playstation 4", 2.799, 10));
+		product.add(new Produto("G1-10", "Projetor 4k LG", 7.000, 10));
 
+		for (int x = 0; x < 10; x++) {
+			System.out.println (String.toString){
+			
+			}
+			}	
+	
 	}
-
 }
