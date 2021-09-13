fe945995f699212a4736625bf3721eb3437f4b26
argelcapela <argelcapeladossantos@gmail.com>
2021-09-13 19:00:04 -0300

    aa

--- ecommerceGeneletronJavaPOO/src/start.bat
+++ ecommerceGeneletronJavaPOO/src/start.bat
@@ -1,3 +1,4 @@
+@title Bem Vind(a/o/e) Ao E-Commerce GenEletron!
 @SET JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF8
 @cd ../
 @javac src/app/TelaPrincipal.java src/entities/Produto.java src/entities/NotaFiscal.java src/app/TelaFormaDePagamento.java src/app/TelaDadosDoCliente.java src/app/TelaNotaFiscal.java src/app/TelaInicial.java
